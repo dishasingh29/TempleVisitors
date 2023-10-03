39055da69f7e6cc28dcead51084631ff59cc03af
dishasingh29 <98078584+dishasingh29@users.noreply.github.com>
2023-10-03 15:36:57 +0530

    first commit

---
+++ JSFile/bgDemo.js
@@ -0,0 +1,11 @@
+function signin(){
+    var Drop = document.getElementById("showbox");
+    Drop.classList.toggle("show");
+}
+
+function signup(){
+    var Drop = document.getElementById("showbox2");
+    Drop.classList.toggle("show");
+}
+
+
